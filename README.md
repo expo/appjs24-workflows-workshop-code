6241a6394c85cbe7a882cd1191824c8c0260cf11
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2024-04-30 14:04:31 +0300

    Add first module solution

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # App.js 2024: "Accelerating Development and Distribution Workflows with Expo" starter project
+
 Starting code and solutions for the workshop.
 
 ## Prerequisites
+
 - A local development environment ready for native iOS and Android React Native / Expo development.
   - In general, the [Expo Local App Development requirements](https://docs.expo.dev/guides/local-app-development/) are a good guide to get you to the point where you can run `npx expo run:ios` and `npx expo run:android`.
   - More specific recommendations:
@@ -16,20 +18,25 @@ Starting code and solutions for the workshop.
 - Mac is highly recommended for the full experience (though all exercises have an Android-only track, so it's possible to do most of the exercises without a Mac).
 
 ## Test your setup before the workshop
+
 Do these steps to ensure you'll be able to complete the workshop exercises.
+
 1. Fork and clone this repo.
 
 2. Restore dependencies with
+
 ```
 npm install
 ```
 
 3. Build and run the app on your iOS simulator:
+
 ```
 npx expo run:ios
 ```
 
 4. Build and run the app on your Android emulator:
+
 ```
 npx expo run:android
 ```
@@ -39,6 +46,7 @@ If these steps don't work, refer to the [Expo Local Development guide](https://d
 If you get a mysterious CSS/Tailwind error, refer to [this workaround](https://github.com/marklawlor/nativewind/issues/838#issuecomment-1980957103) and it shouldn't bother you again.
 
 ## About the demo app
+
 The app is a concept for a guide for an art museum, where you can virtually tour the available exhibits and "favorite" the exhibits you would like to see in person.
 
 The works of art themselves are pulled from the [Cleveland Museum of Art Open Access API](https://openaccess-api.clevelandart.org/), retrieved using TanStack query. You could use the API directly, but for reliability's sake, it's pulling the data from local files, though the images themselves are still pulled from the museum's CDN.
@@ -46,6 +54,6 @@ The works of art themselves are pulled from the [Cleveland Museum of Art Open Ac
 The demo is based off the Expo Router tabs starter (`npx create-expo-app --template tabs`), and also includes Nativewind v4 for most of the styling.
 
 ## Talk to the presenters
+
 - [Keith](https://twitter.com/llamaluvr)
 - [Tomek](https://twitter.com/tchayen)
-
---
+++ app/works/[id]/share.tsx
@@ -0,0 +1,112 @@
+import {
+  View,
+  Text,
+  useWindowDimensions,
+  Platform,
+  Button,
+} from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { Image } from "expo-image";
+import { useWorkByIdQuery } from "@/data/hooks/useWorkByIdQuery";
+import { LoadingShade } from "@/components/LoadingShade";
+import * as Sharing from "expo-sharing";
+import ImagePicker from "react-native-image-crop-picker";
+import Marker, {
+  ImageFormat,
+  Position,
+  TextBackgroundType,
+} from "react-native-image-marker";
+import { useState } from "react";
+
+export default function ShareWork() {
+  const dimensions = useWindowDimensions();
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const { data: work, isLoading } = useWorkByIdQuery(id);
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
+
+  async function share() {
+    if (!croppedImage) {
+      return;
+    }
+
+    await Sharing.shareAsync(
+      Platform.OS === "android" ? `file:${croppedImage}` : croppedImage
+    );
+  }
+
+  async function crop() {
+    const image = await ImagePicker.openCropper({
+      path: work.images.web.url,
+      width: 300,
+      height: 300,
+      mediaType: "photo",
+    });
+    const markedImage = await Marker.markText({
+      backgroundImage: {
+        src: image.path,
+        scale: 1,
+      },
+      watermarkTexts: [
+        {
+          text: "#cma",
+          position: {
+            position: Position.bottomRight,
+          },
+          style: {
+            color: "#fff",
+            fontSize: 20,
+            textBackgroundStyle: {
+              type: TextBackgroundType.none,
+              color: "#000",
+              paddingX: 16,
+              paddingY: 6,
+            },
+          },
+        },
+      ],
+      quality: 100,
+      filename: image.filename,
+      saveFormat: ImageFormat.jpg,
+    });
+
+    setCroppedImage(markedImage);
+  }
+
+  const path = croppedImage
+    ? Platform.OS === "android"
+      ? `file:${croppedImage}`
+      : croppedImage
+    : work && work.images.web.url;
+
+  return (
+    <View className="flex-1 bg-shade-1">
+      <Stack.Screen
+        options={{
+          title: "Share",
+        }}
+      />
+      <View className="py-4 px-4 bg-shade-2">
+        <Text className="text-2xl text-center py-4">
+          Share a clip of this work with your friends.
+        </Text>
+        <View
+          style={{
+            height: dimensions.width - 50,
+            width: dimensions.width - 50,
+            alignSelf: "center",
+          }}
+        >
+          <Image
+            source={{ uri: path }}
+            style={{ width: "100%", height: "100%" }}
+            contentFit="cover"
+            transition={500}
+          />
+        </View>
+      </View>
+      <LoadingShade isLoading={isLoading} />
+      <Button onPress={crop} title="Crop" />
+      <Button onPress={share} title="Share" />
+    </View>
+  );
+}
