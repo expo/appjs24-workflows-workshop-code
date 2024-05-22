0e6e740958d3bc1d3666a6a55e4cd84083c74eb9
gerryland <vs.yakhno@gmail.com>
2024-05-22 09:49:12 +0200

    lessn 1

--- app/works/[id]/index.tsx
+++ app/works/[id]/index.tsx
@@ -1,19 +1,13 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  Pressable,
-  useWindowDimensions,
-} from "react-native";
-import { Stack, useLocalSearchParams, Link } from "expo-router";
-import { Image } from "expo-image";
-import { FontAwesome as Icon } from "@expo/vector-icons";
-import { useWorkByIdQuery } from "@/data/hooks/useWorkByIdQuery";
-import { useFavStatusQuery } from "@/data/hooks/useFavStatusQuery";
-import { useFavStatusMutation } from "@/data/hooks/useFavStatusMutation";
-import colors from "@/constants/colors";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { LoadingShade } from "@/components/LoadingShade";
+import { View, Text, ScrollView, Pressable, useWindowDimensions } from 'react-native';
+import { Stack, useLocalSearchParams, Link } from 'expo-router';
+import { Image } from 'expo-image';
+import { FontAwesome as Icon } from '@expo/vector-icons';
+import { useWorkByIdQuery } from '@/data/hooks/useWorkByIdQuery';
+import { useFavStatusQuery } from '@/data/hooks/useFavStatusQuery';
+import { useFavStatusMutation } from '@/data/hooks/useFavStatusMutation';
+import colors from '@/constants/colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { LoadingShade } from '@/components/LoadingShade';
 
 export default function DisplayWork() {
   const dimensions = useWindowDimensions();
@@ -39,17 +33,14 @@ export default function DisplayWork() {
     <View className="flex-1 bg-shade-1">
       <Stack.Screen
         options={{
-          title: work?.title || "Loading...",
+          title: work?.title || 'Loading...'
         }}
       />
-      <ScrollView
-        contentContainerStyle={{ paddingBottom: insets.bottom }}
-        contentContainerClassName="bg-shade-1"
-      >
+      <ScrollView contentContainerStyle={{ paddingBottom: insets.bottom }} contentContainerClassName="bg-shade-1">
         <View className="py-4 px-4 bg-shade-2">
           <Image
             style={{
-              height: dimensions.width,
+              height: dimensions.width
             }}
             source={{ uri: work && work.images.web.url }}
             contentFit="contain"
@@ -58,10 +49,8 @@ export default function DisplayWork() {
         </View>
         <View>
           <View className="flex-row align-middle">
-            <Text className="flex-1 font-semibold text-3xl px-4 py-2 bg-shade-2">
-              {work?.title}
-            </Text>
-            <View className="justify-center px-4 flex-row items-center">
+            <Text className="flex-1 font-semibold text-3xl px-4 py-2 bg-shade-2">{work?.title}</Text>
+            <View className="justify-center px-6 gap-x-3 flex-row items-center">
               <Pressable
                 className="active:opacity-50"
                 disabled={favQuery.isLoading || favMutation.isPending}
@@ -69,18 +58,15 @@ export default function DisplayWork() {
                   favMutation.mutate({ id: id!, status: !isFav });
                 }}
               >
-                <Icon
-                  name={isFav ? "star" : "star-o"}
-                  color={colors.tint}
-                  size={28}
-                />
+                <Icon name={isFav ? 'star' : 'star-o'} color={colors.tint} size={28} />
               </Pressable>
+              <Link  push href={`/works/${id}/share`}>
+                <Icon name="share-alt" color={colors.tint} size={28} />
+              </Link>
             </View>
           </View>
           <View className="px-4 gap-y-2 py-2">
-            {work?.creators.length ? (
-              <Text className="text-l">{work.creators[0].description}</Text>
-            ) : null}
+            {work?.creators.length ? <Text className="text-l">{work.creators[0].description}</Text> : null}
             <Text className="text-l">{work?.date_text}</Text>
             <Text className="text-l">{work?.technique}</Text>
           </View>
@@ -94,9 +80,7 @@ export default function DisplayWork() {
           )}
           {work?.did_you_know && (
             <>
-              <Text className="text-xl font-semibold px-4 py-2 bg-shade-2">
-                Did you know?
-              </Text>
+              <Text className="text-xl font-semibold px-4 py-2 bg-shade-2">Did you know?</Text>
               <View className="px-4 gap-y-2 py-2">
                 <Text className="text-l">{work.did_you_know}</Text>
               </View>
@@ -110,5 +94,5 @@ export default function DisplayWork() {
 }
 
 function stripTags(htmlish: string) {
-  return htmlish.replace(/<[^>]*>?/gm, "");
+  return htmlish.replace(/<[^>]*>?/gm, '');
 }
---
+++ app/works/[id]/share.tsx
@@ -0,0 +1,79 @@
+import { View, Text, useWindowDimensions, Pressable, Platform } from 'react-native';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { Image } from 'expo-image';
+import * as Sharing from 'expo-sharing';
+import ImagePicker from 'react-native-image-crop-picker';
+import { useWorkByIdQuery } from '@/data/hooks/useWorkByIdQuery';
+import { LoadingShade } from '@/components/LoadingShade';
+import { useState } from 'react';
+
+function normalizeFilePath(path: string) {
+  if (Platform.OS === 'android' && !path.startsWith('file://')) {
+    return `file://${path}`;
+  }
+  return path;
+}
+
+export default function ShareWork() {
+  const dimensions = useWindowDimensions();
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const { data: work, isLoading } = useWorkByIdQuery(id!);
+
+  const [editedImagePath, setEditedImagePath] = useState<string | undefined>(undefined);
+
+  async function share() {
+    await Sharing.shareAsync(editedImagePath || work.images.web.url);
+  }
+
+  async function crop() {
+    const image = await ImagePicker.openCropper({
+      path: work.images.web.url,
+      width: 300,
+      height: 300,
+      mediaType: 'photo'
+    });
+    setEditedImagePath(normalizeFilePath(image.path));
+  }
+
+  return (
+    <View className="flex-1 bg-shade-1">
+      <Stack.Screen
+        options={{
+          title: 'Share'
+        }}
+      />
+      <View className="py-4 px-4 bg-shade-2 gap-3">
+        <Text className="text-2xl text-center">Share a clip of this work with your friends.</Text>
+        <View
+          style={{
+            height: dimensions.width - 50,
+            width: dimensions.width - 50,
+            alignSelf: 'center'
+          }}
+        >
+          <Image
+            source={{ uri: editedImagePath ? editedImagePath : work && work.images.web.url }}
+            style={{ width: '100%', height: '100%' }}
+            contentFit="cover"
+            transition={500}
+          />
+        </View>
+        <RoundButton onPress={crop} title="Crop" />
+        <RoundButton title="Share" disabled={!editedImagePath} onPress={share} />
+      </View>
+      <LoadingShade isLoading={isLoading} />
+    </View>
+  );
+}
+
+function RoundButton({ title, onPress, disabled = false }: { title: string; onPress: () => void; disabled?: boolean }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      disabled={disabled}
+      className={`py-2 rounded-md active:opacity-50 ${disabled ? 'bg-gray-500' : 'bg-tint'}`}
+    >
+      <Text className="text-xl text-center text-white">{title}</Text>
+    </Pressable>
+  );
+}
