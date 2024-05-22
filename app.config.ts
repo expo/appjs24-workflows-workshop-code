0ed085ae826842a02c29d8f31524da693f6aafd7
Keith Kurak <keith.kurak@gmail.com>
2024-05-22 14:04:52 +0200

    update app config with variants

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,11 @@
 import "ts-node/register";
 import { ExpoConfig } from "expo/config";
 
+const IS_DEV = process.env.APP_VARIANT === "development";
+
 module.exports = ({ config }: { config: ExpoConfig }) => {
   return {
-    name: "Art Museum",
+    name: IS_DEV ? "Art Museum (dev)" : "Art Museum",
     slug: "appjs24-workflows-workshop-code",
     version: "1.0.0",
     orientation: "portrait",
@@ -18,7 +20,7 @@ module.exports = ({ config }: { config: ExpoConfig }) => {
     assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "com.expo.appjs24-workflows-workshop-code",
+      bundleIdentifier: "com.expo.appjs24-workflows-workshop-code" + (IS_DEV ? "-dev" : ""),
       entitlements: {
         "com.apple.security.application-groups": [
           "group.appjs24-workflows-workshop-code",
@@ -30,7 +32,7 @@ module.exports = ({ config }: { config: ExpoConfig }) => {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
-      package: "com.expo.appjs24workflowsworkshopcode",
+      package: "com.expo.appjs24workflowsworkshopcode" + (IS_DEV ? "dev" : ""),
     },
     web: {
       bundler: "metro",
@@ -44,8 +46,8 @@ module.exports = ({ config }: { config: ExpoConfig }) => {
     experiments: {
       typedRoutes: true,
     },
-    runtimeVersion: {
-      policy: "appVersion",
-    },
+    "runtimeVersion": {
+      "policy": "appVersion"
+    }
   };
 };
